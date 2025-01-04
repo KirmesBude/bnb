c1b2d9ec23235da381c346b6ffc7d355e168705f
KirmesBude <kirmesbude@gmail.com>
2025-01-04 20:40:37 +0100

    Rough state layout

---
+++ src/game.rs
@@ -0,0 +1,117 @@
+use bevy::prelude::*;
+use bevy_inspector_egui::quick::StateInspectorPlugin;
+
+pub struct GamePlugin;
+
+impl Plugin for GamePlugin {
+    fn build(&self, app: &mut App) {
+        app.init_state::<AppState>().add_sub_state::<RoundState>();
+
+        app.register_type::<AppState>()
+            .register_type::<RoundState>()
+            .register_type::<Round>();
+
+        AppState::setup(app);
+        RoundState::setup(app);
+
+        app.add_plugins(StateInspectorPlugin::<AppState>::default());
+        app.add_plugins(StateInspectorPlugin::<RoundState>::default());
+    }
+}
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States, Reflect)]
+enum AppState {
+    #[default]
+    Menu,
+    InGame,
+}
+
+impl AppState {
+    fn setup(app: &mut App) -> &mut App {
+        app.add_systems(Update, menu_transition.run_if(in_state(AppState::Menu)))
+    }
+}
+
+fn menu_transition(mut next_state: ResMut<NextState<AppState>>) {
+    next_state.set(AppState::InGame);
+}
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, SubStates, Reflect)]
+#[source(AppState = AppState::InGame)]
+enum RoundState {
+    #[default]
+    Init,
+    StartOfRoundEffects,
+    CardSelection,
+    OrderingInitiative,
+    CharacterAndMonsterTurns,
+    EndOfRound,
+}
+
+impl RoundState {
+    fn setup(app: &mut App) -> &mut App {
+        app.add_systems(OnEnter(RoundState::Init), init_on_enter)
+            .add_systems(Update, init_transition.run_if(in_state(RoundState::Init)))
+            .add_systems(
+                OnEnter(RoundState::StartOfRoundEffects),
+                start_of_round_effects_on_enter,
+            )
+            .add_systems(
+                Update,
+                start_of_round_effects_transition.run_if(in_state(RoundState::StartOfRoundEffects)),
+            )
+            .add_systems(
+                Update,
+                card_selection_transition.run_if(in_state(RoundState::CardSelection)),
+            )
+            .add_systems(
+                Update,
+                ordering_initiative_transition.run_if(in_state(RoundState::OrderingInitiative)),
+            )
+            .add_systems(
+                Update,
+                character_and_monster_turns_transition
+                    .run_if(in_state(RoundState::CharacterAndMonsterTurns)),
+            )
+            .add_systems(
+                Update,
+                end_of_round_transition.run_if(in_state(RoundState::EndOfRound)),
+            )
+    }
+}
+
+#[derive(Debug, Default, Resource, Reflect)]
+#[reflect(Resource)]
+pub struct Round(usize);
+
+fn init_on_enter(mut commands: Commands) {
+    commands.insert_resource(Round::default());
+}
+
+fn init_transition(mut next_state: ResMut<NextState<RoundState>>) {
+    next_state.set(RoundState::StartOfRoundEffects);
+}
+
+fn start_of_round_effects_on_enter(mut round: ResMut<Round>) {
+    round.0 += 1;
+}
+
+fn start_of_round_effects_transition(mut next_state: ResMut<NextState<RoundState>>) {
+    next_state.set(RoundState::CardSelection);
+}
+
+fn card_selection_transition(mut next_state: ResMut<NextState<RoundState>>) {
+    next_state.set(RoundState::OrderingInitiative);
+}
+
+fn ordering_initiative_transition(mut next_state: ResMut<NextState<RoundState>>) {
+    next_state.set(RoundState::CharacterAndMonsterTurns);
+}
+
+fn character_and_monster_turns_transition(mut next_state: ResMut<NextState<RoundState>>) {
+    next_state.set(RoundState::EndOfRound);
+}
+
+fn end_of_round_transition(mut next_state: ResMut<NextState<RoundState>>) {
+    next_state.set(RoundState::StartOfRoundEffects);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 mod creature;
 mod demo;
+mod game;
 mod scenario;
 
 use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use creature::CreaturePlugin;
 use demo::DemoPlugin;
+use game::GamePlugin;
 use scenario::ScenarioPlugin;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, ScenarioPlugin, CreaturePlugin, DemoPlugin))
+        .add_plugins((
+            DefaultPlugins,
+            GamePlugin,
+            ScenarioPlugin,
+            CreaturePlugin,
+            DemoPlugin,
+        ))
+        .add_plugins(WorldInspectorPlugin::new())
         .run();
 }
