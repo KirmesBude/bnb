228bf9bce2c7f8496ee4a8bfe6e42bb22cb7c9ca
KirmesBude <kirmesbude@gmail.com>
2025-01-04 23:43:35 +0100

    Not quite working

--- src/demo.rs
+++ src/demo.rs
@@ -1,7 +1,39 @@
-use bevy::prelude::*;
+use bevy::{color::palettes::css::{BLACK, BLUE, RED, WHITE}, prelude::*};
+
+use crate::scenario::{Map, MapPosition};
 
 pub struct DemoPlugin;
 
 impl Plugin for DemoPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup);
+    }
+}
+
+fn setup(mut commands: Commands) {
+    let map = commands.spawn(Map::new(8, 8)).id();
+
+    commands.spawn((Sprite {
+        color: WHITE.into(),
+        custom_size: Some(Vec2::new(50.0, 50.0)),
+        ..default()
+    }, MapPosition::new(map, 3, 4)));
+
+    commands.spawn((Sprite {
+        color: BLACK.into(),
+        custom_size: Some(Vec2::new(50.0, 50.0)),
+        ..default()
+    }, MapPosition::new(map, 4, 4)));
+
+    commands.spawn((Sprite {
+        color: BLUE.into(),
+        custom_size: Some(Vec2::new(50.0, 50.0)),
+        ..default()
+    }, MapPosition::new(map, 3, 3)));
+
+    commands.spawn((Sprite {
+        color: RED.into(),
+        custom_size: Some(Vec2::new(50.0, 50.0)),
+        ..default()
+    }, MapPosition::new(map, 3, 2)));
 }
--- src/scenario.rs
+++ src/scenario.rs
@@ -7,11 +7,13 @@ pub struct ScenarioPlugin;
 
 impl Plugin for ScenarioPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, change_map_position_update);
+        app.add_systems(Update, (map_position_add, change_map_position_update, sync_position_to_transform).chain());
+        app.register_type::<MapPosition>();
     }
 }
 
 #[derive(Debug, Component)]
+#[require(Transform)]
 pub struct Map {
     width: usize,
     height: usize,
@@ -48,8 +50,9 @@ impl Map {
     }
 }
 
-#[derive(Debug, Component)]
-#[component(on_add = MapPosition::on_add, on_remove = MapPosition::on_remove)]
+#[derive(Debug, Component, Reflect)]
+#[reflect(Component)]
+#[component(on_remove = MapPosition::on_remove)]
 pub struct MapPosition {
     map: Entity,
     x: usize,
@@ -88,16 +91,6 @@ impl MapPosition {
         self.y
     }
 
-    pub fn on_add(mut world: DeferredWorld, entity: Entity, _: ComponentId) {
-        let (x, y) = {
-            let component = world.get::<Self>(entity).unwrap();
-            (component.x, component.y)
-        };
-        let mut map = world.get_mut::<Map>(entity).unwrap();
-
-        map.add(entity, x, y);
-    }
-
     pub fn on_remove(mut world: DeferredWorld, entity: Entity, _: ComponentId) {
         let (x, y) = {
             let component = world.get::<Self>(entity).unwrap();
@@ -109,6 +102,17 @@ impl MapPosition {
     }
 }
 
+pub fn map_position_add(
+    mut maps: Query<&mut Map>,
+    map_positions: Query<(Entity, &MapPosition), Added<MapPosition>>,
+) {
+    for (entity, map_position) in &map_positions {
+        if let Ok(mut map) = maps.get_mut(map_position.map) {
+            map.add(entity, map_position.x, map_position.y);
+        }
+    }
+}
+
 pub fn change_map_position_update(
     mut maps: Query<&mut Map>,
     map_positions: Query<(Entity, &MapPosition), Changed<MapPosition>>,
@@ -116,6 +120,20 @@ pub fn change_map_position_update(
     for (entity, map_position) in &map_positions {
         if let Ok(mut map) = maps.get_mut(map_position.map) {
             map.update(entity, map_position);
+        }
+    }
+}
+
+/* TODO: Should use hierarchy */
+fn sync_position_to_transform(
+    maps: Query<(&GlobalTransform, &Map)>,
+    mut map_positions: Query<(&mut Transform, &MapPosition)>
+) {
+    for (mut transform, map_position) in &mut map_positions {
+        if let Ok((map_transform, map)) = maps.get(map_position.map) {
+            /* TODO: Does not support map rotation */
+            let tile_size = 50.0;
+            transform.translation = map_transform.translation() + Vec3::new(((map_position.x as f32)-((map.width as f32)/2.0)) * tile_size, ((map_position.y as f32)-((map.height as f32)/2.0)) * tile_size, 0.0);
         }
     }
 }
